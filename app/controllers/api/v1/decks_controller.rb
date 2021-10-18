72095fae48915da8ca702c63fe32612fde16c29c
Bashu Naimi-Roy <bashunaimiroy@gmail.com>
2021-10-17 20:35:20 -0400

    add simple JSON return from /decks

--- app/controllers/api/v1/decks_controller.rb
+++ app/controllers/api/v1/decks_controller.rb
@@ -1,6 +1,10 @@
 module API
     module V1
         class DecksController < ApplicationController
+            def index
+                @decks = Deck.order("created_at DESC")
+                render json: { status: 'SUCCESS', message: 'these are all of the decks', data: @decks }, status: :ok
+            end
         end
     end
 end
