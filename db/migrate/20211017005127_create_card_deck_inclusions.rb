a7f703a77667caf447174f6699aefa5c02abc975
Bashu Naimi-Roy <bashunaimiroy@gmail.com>
2021-10-17 20:00:44 -0400

    update syntax for card deck inclusions migration

--- db/migrate/20211017005127_create_card_deck_inclusions.rb
+++ db/migrate/20211017005127_create_card_deck_inclusions.rb
@@ -1,8 +1,8 @@
 class CreateCardDeckInclusions < ActiveRecord::Migration[6.1]
   def change
     create_table :card_deck_inclusions, id:false do |t|
-      t.references(:card, null: false, type: :string, index: false, foreign_key: true)
-      t.references(:deck, null: false, foreign_key: true)
+      t.references :card, null: false, type: :string, index: false, foreign_key: true
+      t.references :deck, null: false, foreign_key: true
       t.integer :quantity
       t.timestamps
     end
