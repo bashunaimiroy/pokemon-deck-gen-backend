72c394e1ceef150e720f964f68a99e589345696e
Bashu Naimi-Roy <bashunaimiroy@gmail.com>
2021-10-17 20:01:10 -0400

    add deck/card/inclusions seeds for dev environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+case Rails.env
+when 'development'
+
+  pokemon_types = [
+    "Colorless",
+    "Darkness",
+    "Dragon",
+    "Fairy",
+    "Fighting",
+    "Fire",
+    "Grass",
+    "Lightning",
+    "Metal",
+    "Psychic",
+    "Water"
+  ]
+
+  pokemon_types.each do |type| 
+    deck_of_type = Deck.create(pokemon_type: type)
+    energy_of_type = Card.create(name: "#{type} Energy", id: "#{type}-en")
+    pokemon_of_type = Card.create(name: "#{type}-type Pokemon", id: "#{type}-pk")
+    CardDeckInclusion.create(deck_id: deck_of_type.id, card_id: energy_of_type.id, quantity: 10 )
+    CardDeckInclusion.create(deck_id: deck_of_type.id, card_id: pokemon_of_type.id, quantity: 1 )
+  end
+  
+end
