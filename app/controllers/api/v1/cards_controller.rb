2b9a52042e8defeecb0b5aeea68c6de736e225e4
Bashu Naimi-Roy <bashunaimiroy@gmail.com>
2021-10-20 17:46:10 -0400

    add Show action for Cards Controller

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,6 +1,15 @@
 module API
     module V1
         class CardsController < ApplicationController
+            def show
+                card_id = params[:id]
+                @card = Pokemon::Card.find(card_id)
+                render json: { 
+                    status: 'SUCCESS', 
+                    message: "Card with ID #{card_id} found", 
+                    data: @card
+                }, status: :ok
+            end
         end
     end
 end
