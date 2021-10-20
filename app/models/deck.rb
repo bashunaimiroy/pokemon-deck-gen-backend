9c482492662e4b5f7bd8c5e0e7231835e5249f88
Bashu Naimi-Roy <bashunaimiroy@gmail.com>
2021-10-20 16:55:55 -0400

    add Constraint constants to deck model

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,4 +1,10 @@
 class Deck < ApplicationRecord
+    # constants
+    CONSTRAINTS = {
+        deck_size: 60,
+        energy_card_count: 10
+    }
+
     scope :filter_by_pokemon_type, -> (pokemon_type) { where pokemon_type: pokemon_type }
     validates :pokemon_type, presence: true
     has_many :card_deck_inclusions
